c1eec292dcf67df79acf63e361861ff4f87791e9
saikrishna575 <saikrishna575@gmail.com>
2016-03-12 23:40:32 -0500

    Registration Done

--- LeagueManagement/Models/AccountViewModels.cs
+++ LeagueManagement/Models/AccountViewModels.cs
@@ -79,6 +79,19 @@ namespace LeagueManagement.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+       
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+
     }
 
     public class ResetPasswordViewModel
